139f633645d9038d1d2dfac1677ea7b7b6a50e44
hiroki-osuga68 <osugahi1006@keio.jp>
2022-05-07 15:56:03 +0900

    #28ではなく、#30の作業内容と、#20完了

--- teamdev-2022-sample2/mysql/docker-entrypoint-initdb.d/init.sql
+++ teamdev-2022-sample2/mysql/docker-entrypoint-initdb.d/init.sql
@@ -7,38 +7,74 @@ USE shukatsu;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 INSERT INTO
-  users
+	users
 SET
-  email = 'test@posse-ap.com',
-  password = sha1('password');
+	email = 'test@posse-ap.com',
+	password = sha1('password');
 
 DROP TABLE IF EXISTS events;
 
 CREATE TABLE events (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO events SET title = 'イベント1';
+
+INSERT INTO events SET title = 'イベント2';
+
+INSERT INTO events SET title = 'イベント3';
+
+DROP TABLE IF EXISTS agents;
+
+CREATE TABLE agents (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	agent_name VARCHAR(255) NOT NULL,
+	agent_url VARCHAR(8190) NOT NULL,
+	representative VARCHAR(255) NOT NULL,
+	contractor VARCHAR(255) NOT NULL,
+	department VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	phone_number VARCHAR(255) UNIQUE NOT NULL,
+	address VARCHAR(255) UNIQUE NOT NULL,
+	post_period DATETIME NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	deleted_at INT DEFAULT 0
 );
 
 INSERT INTO
-  events
-SET
-  title = 'イベント1';
-
-INSERT INTO
-  events
-SET
-  title = 'イベント2';
-INSERT INTO
-  events
-SET
-  title = 'イベント3';
+	agents (
+		agent_name,
+		agent_url,
+		representative,
+		contractor,
+		department,
+		email,
+		phone_number,
+		address,
+		post_period,
+		deleted_at
+	)
+VALUES
+	(
+		"ラクナビ",
+		"rakunavi.com",
+		"田中太郎",
+		"佐藤次郎",
+		"営業",
+		"rakunavi@gmail.com",
+		"0120-000-000",
+		"サンプル",
+		"2023-05-05"
+	);
--- teamdev-2022-sample2/src/public/js/app.js
+++ teamdev-2022-sample2/src/public/js/app.js
@@ -14,27 +14,27 @@
 
 const tags = document.querySelectorAll('.tag');
 
-tags.forEach(tag =>{
-tag.addEventListener('click', function(){
-        // console.log(this);
-        tag.classList.toggle('bi-check-lg');
-    })
+tags.forEach(tag => {
+   tag.addEventListener('click', function () {
+      // console.log(this);
+      tag.classList.toggle('bi-check-lg');
+   })
 });
 
 // 指定箇所へのスムーススクロール
-$(function(){
+$(function () {
    // #で始まるアンカーをクリックした場合に処理
-   $('a[href^=#]').click(function() {
+   $('a[href^=#]').click(function () {
       // スクロールの速度
       var speed = 400; // ミリ秒
       // アンカーの値取得
-      var href= $(this).attr("href");
+      var href = $(this).attr("href");
       // 移動先を取得
       var target = $(href == "#" || href == "" ? 'html' : href);
       // 移動先を数値で取得
       var position = target.offset().top;
       // スムーススクロール
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      $('body,html').animate({ scrollTop: position }, speed, 'swing');
       return false;
    });
 });
@@ -42,11 +42,34 @@ $(function(){
 // ドロップダウンメニュー項目を押してもドロップダウンメニューが閉じないようにする
 $('.dropdown-item').on('click.bs.dropdown.data-api', (event) => event.stopPropagation())
 
+// モーダル
+$(function () {
+   $('.js-modal-open').on('click', function () {
+      $('.js-modal').fadeIn();
+      return false;
+   });
+   $('.js-modal-close').on('click', function () {
+      $('.js-modal').fadeOut();
+      return false;
+   });
+});
 // ---------------Miu's area----------------------------------------------
 
 
 // 検索結果画面：キープボタンクリック時の☆アイコン切り替え
-$('.keep-btn').click(function(){
-     $('.white-star').hide();
-     $('.black-star').show();
-});
+// $('.keep-btn').click(function(){
+//      $('.white-star').hide();
+//      $('.black-star').show();
+// });
+
+let changeStars = document.querySelectorAll('.keep-btn');
+
+changeStars.forEach(changeStar => {
+   changeStar.addEventListener('click', function () {
+      console.log("a")
+      changeStar.classList.remove('bi-star');
+      changeStar.classList.remove('white-star');
+      changeStar.classList.add('bi-star-fill');
+      changeStar.classList.add('black-star');
+   })
+})
